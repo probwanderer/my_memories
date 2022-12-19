e561c5efc593d0c7324dbb94216bb14fa9917cbb
probwanderer <52926438+probwanderer@users.noreply.github.com>
2022-12-19 17:16:12 +0530

    CRUD Complete

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,10 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import useStyles from './styles';
 import FileBase from 'react-file-base64';
-import {useDispatch} from 'react-redux';
-import { createPost } from '../../actions/posts';
-const Form = () => {
+import {useDispatch,useSelector } from 'react-redux';
+import { createPost,updatePost } from '../../actions/posts';
+const Form = ({currentId,setCurrentId}) => {
     const [postData,setPostData] = useState({
         creator:'',
         title:'',
@@ -12,29 +12,46 @@ const Form = () => {
         tags:'',
         selectedFile:''
     });
-    const dispatch=useDispatch()
+   
+ const post = useSelector((state)=> currentId?state.posts.find((p)=>p._id===currentId):null);
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        if(post) setPostData(post);
+    },[post])
 
     const classes = useStyles();
     const clear = () => {
-        
+        setCurrentId(null);
+        setPostData({creator:'',
+        title:'',
+        message:'',
+        tags:'',
+        selectedFile:''});
     }
     const handleSubmit = (e) =>{
         e.preventDefault();
-        dispatch(createPost(postData));
+        if(currentId){
+            dispatch(updatePost(currentId,postData));
+        }
+        else{
+            dispatch(createPost(postData));
+        }
+     clear();
+       
     }
     return (
         <Paper className= { classes.paper }>
             <form autoComplete = "off" noValidate className={`${classes.root} ${classes.form}`} onSubmit ={handleSubmit}>
-            <Typography variant="h6" >Creating a memory</Typography>
+            <Typography variant="h6" >{currentId?'Editing':'Creating'} a memory</Typography>
             <TextField name="creator" variant="outlined" label="Creator"  full Width value={postData.creator} onChange={(e)=>setPostData({...postData,creator:e.target.value})} />
             <TextField name="title" variant="outlined" label="Title"  full Width value={postData.title} onChange={(e)=>setPostData({...postData,title:e.target.value})} />
             <TextField name="message" variant="outlined" label="Message"  full Width value={postData.message} onChange={(e)=>setPostData({...postData,message:e.target.value})} />
-            <TextField name="tags" variant="outlined" label="Tags"  full Width value={postData.tags} onChange={(e)=>setPostData({...postData,tags:e.target.value})} />
+            <TextField name="tags" variant="outlined" label="Tags"  full Width value={postData.tags} onChange={(e)=>setPostData({...postData,tags:e.target.value.split(',')})} />
             <div className ={classes.fileInput}>
                 <FileBase
                     type="file"
-                    multiple="false"
-                    onDone={({base64})=>({...postData,selectedFile:base64})}
+                    multiple={false}
+                    onDone={({base64})=>setPostData({...postData,selectedFile:base64})}
                 />
             </div>
             <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,18 +4,16 @@ import {Grid,CircularProgress} from '@material-ui/core'
 import { useSelector } from 'react-redux';
 import useStyles from './styles';
 
-const Posts = () => {
+const Posts = ({setCurrentId}) => {
     const classes = useStyles();
     const posts = useSelector((state)=> state.posts);
-    console.log(posts);
-
     return (
        !posts.length?<CircularProgress/>:(
         <Grid className={classes.container} container alignItems="stretch" spacing={3}>
             {
                 posts.map((post)=>(
                     <Grid key={post._id} item xs={12} sm={6}>
-                        <Post post={post}/>
+                        <Post post={post} setCurrentId={setCurrentId}/>
                     </Grid>
                 ))
             }
